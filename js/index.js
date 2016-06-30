36d4dfd34ded30b1665d033d8dd09b3ba21a552c
yoku2010 <yoku2010@gmail.com>
2016-06-30 19:13:19 +0530

    New Details

--- js/index.js
+++ js/index.js
@@ -4,12 +4,16 @@ $(function(){
         img: "img/ankit-bw.jpg",
         themeColor: "Green",
         name: "Ankit Prasad",
-        title: "Software Developer",
-        description: "I am a tech enthusiast who can't get enough of his Moto x play. You'll usually find me typing away furiously on my keyboard while eating a packet of chips (just one packet a day, no more). If, for some strange reason, i am not writing or reading up on technology, you’ll find me listening to music on my phone or laptop while trying to ensure my hair is all spiked up. I do hope you fine chaps enjoy my resume as much as I do.",
+        title: " Software Developer",
+        description: "I am a tech enthusiast who can't get enough of his Moto x play. You'll usually find me typing away furiously on my keyboard while eating a packet of chips (just one packet a day, no more).",
         socialLinks: [
             {
                 icon: "fa-facebook",
                 href: "https://www.facebook.com/ankitprasad29/"
+            },
+            {
+                icon: "fa-linkedin",
+                href: "https://www.linkedin.com/in/ankit-prasad-4052172b"
             },
             {
                 icon: "fa-github",
@@ -18,6 +22,31 @@ $(function(){
             {
                 icon: "fa-graduation-cap",
                 href: "http://imankitpd.github.io/myresume/"
+            }
+        ]
+    });
+    $("#quick_info_harish").quickInfo({
+        img: "img/harish-bw.jpg",
+        themeColor: "Blue",
+        name: "Harish Sharma",
+        title: " Network Engineer",
+        description: "\"patience, persistence and perspiration make an unbeatable combination for success.\"",
+        socialLinks: [
+            {
+                icon: "fa-facebook",
+                href: "https://www.facebook.com/profile.php?id=100003743379637"
+            },
+            {
+              icon: "fa-linkedin",
+              href: "https://www.linkedin.com/in/harish-kumar-49b4b9b2"
+            },
+            {
+                icon: "fa-github",
+                href: "https://github.com/harishsha/"
+            },
+            {
+                icon: "fa-graduation-cap",
+                href: "http://harishsha.github.io/"
             }
         ]
     });
